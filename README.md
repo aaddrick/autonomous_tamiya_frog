59b7efbbb62d2ef01bb01c14e0e686db1a98ae97
aaddrick <aaddrick@gmail.com>
2019-04-16 21:21:13 -0400

    General Updates

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # autonomous_tamiya_frog
 Autonomizing a Tamiya Frog using Arduino
 
-test
+
+
+TODO Clean up this README with proper markdown...
+
+Scrap book for the code:
+
+Throttle and steering control:
+
+  millis()
+  "Returns the number of milliseconds passed since the Arduino board
+  began running the current program. This number will overflow (go
+  back to zero), after approximately 50 days."
+
+  Using delay() stops the program and everything has to wait.
+
+  1. servo_time starts out at 0
+  2. You use millis() to find out how long the board has been running.
+  3. Subtract servo_time from millis() and see if it's greater than your
+    SERVO_SPEED variable.
+  4. If more milliseconds have passed than your SERVO_SPEED
+    4a. servo_time is updated to the current millis()
+    4b. action is executed
+  5. Return to step 2.
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Servo.h>
 
-#define SERVO_SPEED 25    // minimum number of milliseconds per step
 #define STEERING_STEP 3   //desired incremental step for steering in degrees
 #define THROTTLE_STEP 3   //desired incremental step for throttle in degrees
 
 int steering_pos;  // desired angle for steering
 int throttle_pos;  // desired angle for throttle
+
 
 void setup()
 {
@@ -14,7 +14,7 @@ void setup()
 
   // set the servos to center position and wait 5 seconds to arm ESM
 
-  // TODO test whether or not is needed specifically to arm the ESC.
+  // TODO test whether or not the below chunk is needed specifically to arm the ESC.
   // if so, does it need to be both the steering and throttle, or just the throttle?
   // if not, what happens to steering.read() when there is no set value?
 
@@ -25,46 +25,42 @@ void setup()
 
 void loop()
 {
- DriveControl();
+  // TODO sensor function - first check what you see
+  // TODO planning function - Then tie it into what you're doing
+  ThrottleControl(throttle_pos);
+  SteeringControl(steering_pos);
 }
 
-
-void DriveControl() // TODO break this out into separate functions and speed variables for throttle and steering
+void SteeringControl(steering_time)
 {
-// http://forum.arduino.cc/index.php?topic=4854.msg37047#msg37047
+  static int steering_speed = 25; // minimum number of milliseconds per step
+  static unsigned long steering_time; // placeholder for the last time steering updated.
 
-/*
-  millis()
-  "Returns the number of milliseconds passed since the Arduino board
-  began running the current program. This number will overflow (go
-  back to zero), after approximately 50 days."
+  if ((millis()-steering_time) >= steering_speed)
+  {
+    steering_time = millis(); // save time reference for next position update
 
-  Using delay() stops the program and everything has to wait.
+    // update steering position
+    // if desired position is different from current position move one step left or right
+    // TODO is positive or negative left?
+    if (steering_pos > steering.read()) steering.write(steering.read() + STEERING_STEP);
+    else if (steering_pos < steering.read()) steering.write(steering.read() - STEERING_STEP);
+  }
+}
 
-  1. servo_time starts out at 0
-  2. You use millis() to find out how long the board has been running.
-  3. Subtract servo_time from millis() and see if it's greater than your
-    SERVO_SPEED variable.
-  4. If more milliseconds have passed than your SERVO_SPEED
-    4a. servo_time is updated to the current millis()
-    4b. action is executed
-  5. Return to step 2.
-*/
+void ThrottleControl(throttle_time)
+{
+  static int throttle_speed = 25; // minimum number of milliseconds per step
+  static unsigned long throttle_time;  // placeholder for the last time throttle updated. Used in ThrottleControl()
 
- static unsigned long servo_time;
+  if ((millis()-throttle_time) >= throttle_speed)
+  {
+    throttle_time = millis(); // save time reference for next position update
 
- // check time since last servo position update
- if ((millis()-servo_time) >= SERVO_SPEED)
- {
-   servo_time = millis(); // save time reference for next position update
-
-   // update steering position
-   // if desired position is different from current position move one step left or right
-   if (steering_pos > steering.read()) steering.write(steering.read() + STEERING_STEP);
-   else if (steering_pos < steering.read()) steering.write(steering.read() - STEERING_STEP);
-
-   // update throttle position
-   // if desired position is different from current position move one step left or right
-   if (throttle_pos > throttle.read()) throttle.write(throttle.read() + THROTTLE_STEP);
-   else if (throttle_pos < throttle.read()) throttle.write(throttle.read() - THROTTLE_STEP);
- }
+    // update throttle position
+    // if desired position is different from current position move one step faster or slower.
+    // TODO is positive or negative more throttle?
+    if (throttle_pos > throttle.read()) throttle.write(throttle.read() + THROTTLE_STEP);
+    else if (throttle_pos < throttle.read()) throttle.write(throttle.read() - THROTTLE_STEP);
+  }
+}
